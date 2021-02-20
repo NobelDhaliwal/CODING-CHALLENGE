a2da64002d3aea309e1052a9ca72cc2e91759ffd
nobel <nobelinderpreet91@gmail.com>
2021-02-20 16:22:49 +1100

    Test App created

---
+++ src/Component/Box.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class Box extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChangeColor = this.handleChangeColor.bind(this);
+  }
+
+  // Handler callback
+  handleChangeColor() {
+    // Call parent component changeColor  method passing the color value of div
+    this.props.changeColor(this.props.color);
+  }
+
+  render() {
+    return (
+      <div
+        //  click handler to the div and pass a callback
+        onClick={this.handleChangeColor}
+        style={{
+          backgroundColor: this.props.color,
+          width: "85px",
+          height: "600px",
+        }}
+      />
+    );
+  }
+}
+
+export default Box;
